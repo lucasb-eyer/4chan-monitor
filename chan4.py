b98c8f2b6cd0fa525777f287caff55b770247643
lucasb-eyer <lucasb.eyer.be@gmail.com>
2017-08-23 04:59:57 +0200

    Many changes I don't remember.
    Mostly making it more robust and gentle for long-term collection, I believe.

--- chan4.py
+++ chan4.py
@@ -1,6 +1,7 @@
 from os.path import join as pjoin
 from collections import OrderedDict
 from time import time
+from logging import warning
 
 try:
     from StringIO import StringIO
@@ -8,7 +9,42 @@ except ImportError:
     from io import StringIO
 
 import requests
+import socket  # Just for the `timeout` exception :(
 from bs4 import BeautifulSoup
+
+
+def safeget_json(url):
+    """
+    Get a json while nicely handling (warning about) any types of weird
+    behaviors that can come from the server in real-life.
+
+    Returns `None` for "nothing here, but try again later", `404` for a
+    thread that 404ed and any other json object if the response is good.
+    """
+    try:
+        r = requests.get(url, timeout=(3.5, 5))
+    except (requests.Timeout, socket.timeout, TimeoutError) as e:
+        warning("timeout for {url} (exception type: {et})".format(url=url, et=str(type(e))))
+        return
+    except ConnectionError:
+        warning("other connection error for {url}".format(url=url))
+        return
+
+    if r.status_code == 404:
+        return 404
+    elif r.status_code != 200:
+        warning("return code {code} for {url}".format(code=r.status_code, url=url))
+        return
+
+    try:
+        json = r.json()
+        if json is None:
+            raise ValueError()
+    except ValueError:
+        warning("got invalid JSON for {url}".format(url=url))
+        return
+
+    return json
 
 
 def threads(board='b', pages=[1]):
@@ -16,12 +52,13 @@ def threads(board='b', pages=[1]):
     - `board`: The name of the board, without the slashes. E.g. 'b'
     - `pages`: A list or set of page-numbers indicating which pages to include. `None` for all.
     """
-    r = requests.get("http://a.4cdn.org/{board}/threads.json".format(board=board))
+    url = "http://a.4cdn.org/{board}/threads.json".format(board=board)
+    json = safeget_json(url)
     return {
         thread['no']: Thread(board, thread['no'])
-        for page in r.json() if pages is None or page['page'] in pages
+        for page in json if pages is None or page['page'] in pages
         for thread in page['threads']
-    }
+    } if json is not None and json != 404 else {}
 
 
 class Thread:
@@ -31,33 +68,48 @@ class Thread:
         self.url = "http://a.4cdn.org/{board}/thread/{no}.json".format(board=board, no=no)
         self.done = False
         self.posts = OrderedDict()
+
+        # For being gentle.
         self.lastupd8 = 0
+        self.backoff = 1
 
     def update(self):
         # Only check at most once per second.
-        if self.done or time() - self.lastupd8 < 1000:
+        if self.done or time() - self.lastupd8 < self.backoff:
             return
         self.lastupd8 = time()
 
-        r = requests.get(self.url)
-        if r.status_code == 404:
+        json = safeget_json(self.url)
+        if json is None:
+            return
+        elif json == 404:
             self._close()
             return
-        elif r.status_code != 200:
-            print("Warning: return code {code} for {url}".format(r.status_code, self.url))
-            return
+
+        posts = json['posts']
+
+        # If there are no new posts, wait twice longer next time.
+        self.backoff = 1 if len(posts) > len(self.posts) else 1.5*self.backoff
 
         # Add any new posts
-        posts = r.json()['posts']
         for p in posts:
-            if p['no'] not in self.posts:
+            # I've once had a post without number in /mu/.
+            # Not sure what that was, but let's agree to just skip.
+            if 'no' in p and p['no'] not in self.posts:
                 self.posts[p['no']] = Post(self.board, self.no, p)
+
+        # It sometimes happens that the first post is only half-complete, probably race-condition.
+        # It's missing most basic stuff like. In that case, we just skip it and
+        # it will be updated later on. Note it's also been skipped above.
+        if 'no' not in posts[0]:
+            warning("Skipping half-complete OP at {}".format(self.url))
+            return
 
         # Update the OP's meta-info.
         op = posts[0]
         self.posts[op['no']].update(op)
 
-        # Archiving boards don't delete a thread, but close is. This is marked in the OP.
+        # Archiving boards don't delete a thread, but close it. This is marked in the OP.
         if self.posts[op['no']].closed:
             self._close()
 
@@ -76,6 +128,7 @@ class Post:
         # Normalize!
         pi.setdefault('com', '')
 
+        self.board = board
         self.threadno = threadno
         self.info = pi
         self.closed = False
@@ -99,7 +152,10 @@ class Post:
         """
         self.images = pi['images']
         self.replies = pi['replies']
-        self.unique_ips = pi['unique_ips']
+        # This one disappears once a thread gets archived.
+        # It also may shrink sometimes (albeit rarely and usually just by one),
+        # so we'll just track the peak!
+        self.unique_ips = max(getattr(self, 'unique_ips', 0), pi.get('unique_ips', 0))
 
         if pi.get('closed', 0) == 1:
             self.closed = True
@@ -119,8 +175,10 @@ class Post:
         except ValueError:
             if href[0] == href[-1] == '/':
                 pass  # Ignore link to other board. There's no other indicator.
-            # elif 'catalog#s=' in href:
-                # pass  # Also ignore links to searches in the board.
+            elif 'catalog#s=' in href:
+                pass  # That's mostly caused by idiots writing /r/eddit or /t/umblr.
+            elif href == '/mu/catalog':
+                pass  # In /mu/'s sticky.
             else:
-                print("Can't handle quote:", href, "encountered in", self.threadno)
+                warning("Can't handle quote: {} encountered in thread {} post {}".format(href, self.threadno, self.no))
             return None
--- crawl.py
+++ crawl.py
@@ -7,6 +7,9 @@ import os
 import sys
 import time
 import json
+import logging
+from getopt import getopt
+from itertools import count
 
 # For some reason I have yet to understand (but is probably obvious),
 # updates don't work across processes, but do across threads.
@@ -16,49 +19,83 @@ from multiprocessing.pool import ThreadPool as Pool
 import chan4
 
 
-board = 'b'
-basedir = '/work/lucas/data/4chin'
-
 def update(t):
-    t.update()
+    try:
+        t.update()
+    except (KeyboardInterrupt, SystemExit):
+        raise
+    except:
+        # Log any other exceptions, but continue business.
+        logging.exception("Problem updating thread at {}".format(t.url))
 
+    return t.no if t.done else None
 
 if __name__ == '__main__':
-    threads = {}
-    done = set()
-    pool = Pool(4)
+    # Parse cmdline arguments
+    optlist, boards = getopt(sys.argv[1:], 'ho:j:p:')
+    opts = dict(optlist)
 
-    tdir = "{}/{}/threads".format(basedir, board)
-    pdir = "{}/{}/posts".format(basedir, board)
+    if '-h' in opts or len(boards) == 0:
+        print("Usage: {} [-o OUTDIR] [-j NJOBS] [-p NPAGES] board1 board2 ...".format(os.path.basename(sys.argv[0])))
+        sys.exit(0)
 
-    os.makedirs(tdir, exist_ok=True)
-    os.makedirs(pdir, exist_ok=True)
+    # Dictionary mapping boards to dictionaries mapping tids to threads.
+    # Gotta love that variable name ;)
+    everything = {b: {} for b in boards}
+    ntdone, npdone = 0, 0
+
+    pool = Pool(opts.get('-j'))
+
+    # Create any necessary directories and define utilities for later.
+    basedir = opts.get('-o', os.getcwd())
+    tdir = lambda board: os.path.join(basedir, board, "threads")
+    pdir = lambda board: os.path.join(basedir, board, "posts")
+    for board in boards:
+        os.makedirs(tdir(board), exist_ok=True)
+        os.makedirs(pdir(board), exist_ok=True)
+
+    npages = int(opts.get('-p', 1))
 
     tstart = time.time()
 
-    while True:
+    for e in count():
         t0 = time.time()
-        threads.update(chan4.threads(board, pages=[1]))
 
-        pool.map(update, threads.values())
+        for board, threads in everything.items():
+            # Get new threadlist for this board.
+            front_threads = chan4.threads(board, pages=range(1,npages+1) if e > 0 else None)
 
-        donenow = set()
-        for t in threads.values():
-            # Save the thread once it's done.
-            if t.done:
-                with open('{}/{}.json'.format(tdir, t.no), 'w+') as f:
+            # Keep track of new threads.
+            for tno in front_threads.keys() - threads.keys():
+                threads[tno] = front_threads[tno]
+
+            # Update all known threads in this board.
+            # NOTE: They have some backoff pausing implemented.
+            done = pool.map(update, threads.values())
+            done = [threads[tno] for tno in done if tno is not None]
+
+            # Save and remove the threads that 404ed.
+            for t in done:
+                with open('{}/{}.json'.format(tdir(board), t.no), 'w+') as f:
                     json.dump(t.json(), f)
                 for p in t.posts.values():
-                    with open('{}/{}.json'.format(pdir, p.no), 'w+') as f:
+                    with open('{}/{}.json'.format(pdir(board), p.no), 'w+') as f:
                         json.dump(p.json(), f)
-                donenow.add(t.no)
+            ntdone += len(done)
+            for t in done:
+                del threads[t.no]
+                npdone += len(t.posts)
 
-        for t in donenow:
-            del threads[t]
-            done.add(t)
-
+        # Status report
         t = time.time() - t0
-        sys.stdout.write("\rMonitoring {} threads in {:.1f}s/cycle ({:.2f}s/thread). Completed {} threads in {:.0f}s.".format(len(threads), t, t/len(threads), len(done), time.time() - tstart))
+        nthreads = sum(map(len, everything.values()))
+        sys.stdout.write("\rMonitoring {nthread} threads ({npost} posts) from {nboard} boards in {ct:.1f}s/cycle ({tt:.3f}s/thread). Completed {ntdone} threads ({npdone} posts) in {t:.0f}s ({e}ep).".format(
+            nboard=len(everything),
+            nthread=nthreads,
+            npost=sum(len(t.posts) for ts in everything.values() for t in ts.values()),
+            ntdone=ntdone, npdone=npdone,
+            ct=t, tt=t/nthreads, t=time.time() - tstart, e=e)
+        )
         sys.stdout.flush()
 
         if t < 1:
